32740cf52820aaa99248abe020c9048718f933f5
1gtm <1gtm@appscode.com>
2021-08-24 08:26:01 +0000

    Generate code for provider@v0.0.0-20210622112225-b693562639e1 gen@23925be
    Signed-off-by: 1gtm <1gtm@appscode.com>

--- vendor/kubeform.dev/provider-upcloud-api/apis/server/v1alpha1/server_webhook.go
+++ vendor/kubeform.dev/provider-upcloud-api/apis/server/v1alpha1/server_webhook.go
@@ -20,9 +20,12 @@ package v1alpha1
 
 import (
 	"fmt"
+	"strings"
 
 	base "kubeform.dev/apimachinery/api/v1alpha1"
+	"kubeform.dev/provider-upcloud-api/util"
 
+	jsoniter "github.com/json-iterator/go"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
@@ -38,6 +41,18 @@ func (r *Server) SetupWebhookWithManager(mgr ctrl.Manager) error {
 
 var _ webhook.Validator = &Server{}
 
+var serverForceNewList = map[string]bool{
+	"/network_interface/*/bootable":            true,
+	"/network_interface/*/ip_address":          true,
+	"/network_interface/*/ip_address_family":   true,
+	"/network_interface/*/network":             true,
+	"/network_interface/*/source_ip_filtering": true,
+	"/network_interface/*/type":                true,
+	"/template/*/storage":                      true,
+	"/user_data":                               true,
+	"/zone":                                    true,
+}
+
 // ValidateCreate implements webhook.Validator so a webhook will be registered for the type
 func (r *Server) ValidateCreate() error {
 	return nil
@@ -45,6 +60,53 @@ func (r *Server) ValidateCreate() error {
 
 // ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
 func (r *Server) ValidateUpdate(old runtime.Object) error {
+	if r.Spec.Resource.ID == "" {
+		return nil
+	}
+	newObj := r.Spec.Resource
+	res := old.(*Server)
+	oldObj := res.Spec.Resource
+
+	jsnitr := jsoniter.Config{
+		EscapeHTML:             true,
+		SortMapKeys:            true,
+		TagKey:                 "tf",
+		ValidateJsonRawMessage: true,
+		TypeEncoders:           GetEncoder(),
+		TypeDecoders:           GetDecoder(),
+	}.Froze()
+
+	byteNew, err := jsnitr.Marshal(newObj)
+	if err != nil {
+		return err
+	}
+	tempNew := make(map[string]interface{})
+	err = jsnitr.Unmarshal(byteNew, &tempNew)
+	if err != nil {
+		return err
+	}
+
+	byteOld, err := jsnitr.Marshal(oldObj)
+	if err != nil {
+		return err
+	}
+	tempOld := make(map[string]interface{})
+	err = jsnitr.Unmarshal(byteOld, &tempOld)
+	if err != nil {
+		return err
+	}
+
+	for key := range serverForceNewList {
+		keySplit := strings.Split(key, "/*")
+		length := len(keySplit)
+		checkIfAnyDif := false
+		util.CheckIfAnyDifference("", keySplit, 0, length, &checkIfAnyDif, tempOld, tempOld, tempNew)
+		util.CheckIfAnyDifference("", keySplit, 0, length, &checkIfAnyDif, tempNew, tempOld, tempNew)
+
+		if checkIfAnyDif && r.Spec.UpdatePolicy == base.UpdatePolicyDoNotDestroy {
+			return fmt.Errorf(`server "%v/%v" immutable field can't be updated. To update, change spec.updatePolicy to Destroy`, r.Namespace, r.Name)
+		}
+	}
 	return nil
 }
 
