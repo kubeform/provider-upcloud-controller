8585615e2182db212c396865c3d0a27959822b21
lgtm <1gtm@users.noreply.github.com>
2021-10-28 01:44:38 -0700

    Generate code for provider@v0.0.0-20210622112225-b693562639e1 gen@7da4df3 (#12)
    Signed-off-by: 1gtm <1gtm@appscode.com>

--- vendor/kubeform.dev/provider-upcloud-api/apis/router/v1alpha1/router_types.go
+++ vendor/kubeform.dev/provider-upcloud-api/apis/router/v1alpha1/router_types.go
@@ -51,6 +51,8 @@ type RouterSpec struct {
 	TerminationPolicy base.TerminationPolicy `json:"terminationPolicy,omitempty" tf:"-"`
 
 	ProviderRef core.LocalObjectReference `json:"providerRef" tf:"-"`
+
+	BackendRef *core.LocalObjectReference `json:"backendRef,omitempty" tf:"-"`
 }
 
 type RouterSpecResource struct {
